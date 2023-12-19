0a5706dc934db06ec37d0d1244879cc4f47da17e
cavid.k <cavid.kazimov.98@bk.ru>
2023-12-19 17:54:30 +0400

    add

---
+++ resources/views/edit.blade.php
@@ -0,0 +1,56 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <h1 class="mb-3" style="font-family: Calibri,fantasy">Add your next post below</h1>
+    <div>
+        <div class="card"></div>
+        <div class="main-content md-6">
+            <div class="card-body">
+                <div class="card">
+                    <div class="card-header">
+                        <div class="row">
+                            <div class="col-md-8">
+                                <h4>Edit a post☄</h4>
+                            </div>
+                            <div class="col-md-4 d-flex justify-content-end">
+                                <button type="button" class="btn btn-success">Back</button>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="col-md-7 d-flex justify-content-end">
+                        <div class="card-body"></div>
+                        <form action="">
+                            <div class="card-body"></div>
+                            <div class="form-group">
+                                <label for="" class="form-label">Image</label>
+                                <input type="file" class="form-control" style="width: 200%">
+                            </div>
+                            <div class="form-group">
+                                <label for="" class="form-label">Title</label>
+                                <input type="title" class="form-control" style="width: 200%">
+                            </div>
+                            <div class="form-group">
+                                <label for="" class="form-label">Title</label>
+                                <select name="" class="form-control" style="width: 200%">
+                                    <option value="">Test1</option>
+                                    <option value="">Test2</option>
+                                    <option value="">Test3</option>
+                                    <option value="">Tes4</option>
+                                </select>
+                            </div>
+                            <div class="form-group">
+                                <label for="" class="form-label">Description</label>
+                                <textarea name="" id="" cols="40" rows="5" class="form-control"
+                                          placeholder="add description here"></textarea>
+                            </div>
+                            <div class="col-md-20 d-flex justify-content-center mt-3 mb-2">
+                                <button class="btn btn-primary" type="submit">Submit</button>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+@endsection
---
+++ resources/views/index.blade.php
@@ -0,0 +1,50 @@
+@extends('layouts.master')
+
+@section('content')
+
+    <h1 style="font-family: Calibri,fantasy">This is form index illustrated here</h1>
+    <div class="main-content mt-6">
+        <div class="card">
+            <div class="card-header">All Posts</div>
+        </div>
+
+        <div class="mt-3 mb-3">
+        <div class="col-md-15 d-flex justify-content-end">
+            <a class="btn btn-success mx-1" href="{{route("posts.create")}}">Create</a>
+            <button type="button" class="btn btn-warning">Trashed</button>
+        </div>
+        </div>
+        <div class="card-body">
+            <table class="table table-striped table-bordered border-dark">
+                <thead style="background: #e2e8f0">
+                <tr>
+                    <th scope="col width: 5%">#</th>
+                    <th scope="col" style="width: 20%">Image</th>
+                    <th scope="col" style="width: 30%">Title</th>
+                    <th scope="col" style="width: 10%">Description</th>
+                    <th scope="col" style="width: 10%">Category</th>
+                    <th scope="col" style="width: 10%">Publish Date</th>
+                    <th scope="col" style="height: 10%">Action</th>
+                </tr>
+                </thead>
+                <tbody>
+                <tr>
+                    <th scope="row">1</th>
+                    <td>
+                        <img src="https://picsum.photos/id/237/200/300" width="25%" alt="">
+                    </td>
+                    <td>Lorem Ipsum is simply dummy text.</td>
+                    <td>Lorem ipsum dolor sit amet.</td>
+                    <td>News</td>
+                    <td>2-5-23</td>
+                    <td>
+                        <button type="button" class="btn btn-info">Show</button>
+                        <a class="btn btn-primary mx-1">Edit</a>
+                        <button type="button" class="btn btn-danger">Delete</button>
+                    </td>
+                </tr>
+                </tbody>
+            </table>
+        </div>
+    </div>
+@endsection
