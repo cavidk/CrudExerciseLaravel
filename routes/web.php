55372465090bf6233127ceadf2229d932273a98d
cavid.k <cavid.kazimov.98@bk.ru>
2024-01-15 16:49:29 +0400

    restore route updated
    Took 32 seconds

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,6 @@ Route::get('/', function () {
 });
 
 Route::get('/posts/trash',[PostController::class,'trashed'])->name('posts.trashed');
-Route::post('/posts/{id}/restore', [PostController::class, 'restore'])->name('posts.restore');
+Route::get('/posts/{id}/restore', [PostController::class, 'restore'])->name('posts.restore');
 Route::delete('/posts/{id}/force-delete', [PostController::class, 'forceDelete'])->name('posts.forceDelete');
 Route::resource('posts', PostController::class);
