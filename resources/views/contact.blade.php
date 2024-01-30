b2302decb9dd1b17ff05c9bf65b7e02dd1f4ab6b
cavid.k <cavid.kazimov.98@bk.ru>
2024-01-30 19:01:07 +0400

    up
    Took 1 hour 30 minutes

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,8 +1,14 @@
 @extends('layouts.master')
+
 @section('content')
     <div class="row">
         @foreach($posts as $post)
             <x-post.index :post="$post" />
+            <x-slot name="title">
+                {{$post->title}}
+            </x-slot>
+            <x-slot name="description">
+                {{$post->description}}
         @endforeach
     </div>
 @endsection
